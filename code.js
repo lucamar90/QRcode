e946ffba3fe64a98e455ce60c3ac29a8ae7d5361
Luca Marullo <lvk@live.it>
2022-12-30 15:09:40 +0100

    Initial commit

---
+++ code.js
@@ -0,0 +1,19 @@
+var qrcode = new QRCode("qrcode");
+
+function makeCode () {
+  var elText = document.getElementById("text");
+
+  qrcode.makeCode(elText.value);
+}
+
+makeCode();
+
+$("#text").
+  on("blur", function () {
+    makeCode();
+  }).
+  on("keydown", function (e) {
+    if (e.keyCode == 13) {
+      makeCode();
+    }
+  });
